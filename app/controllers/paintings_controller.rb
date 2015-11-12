3b82eaedb1642078c3c666fda79ed83b9811323d
chloebethcanon <chloebethcanon@gmail.com>
2015-11-11 21:10:34 -0600

    Added HTML to display one painting on individual page

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -3,8 +3,12 @@ class PaintingsController < ApplicationController
   def home
   end
 
-  def paintings
+  def allpaintings
     @all_paintings = Painting.all
+  end
+
+  def eachpainting
+    @each_painting = Painting.last
   end
 
 end
