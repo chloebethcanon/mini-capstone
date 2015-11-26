7ae9feff1bda514466f3c98e1dbdfcb14f77df99
chloebethcanon <chloebethcanon@gmail.com>
2015-11-25 19:51:59 -0600

    Added painting_id to create method, order form and order show page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,7 +4,8 @@ class OrdersController < ApplicationController
 def create
   order = Order.create(
     user_id: current_user.id,
-    quantity: params[:quantity] 
+    quantity: params[:quantity],
+    painting_id: params[:painting_id] 
     )
   flash[:success] = "Order successfully created."
   redirect_to "/orders/#{order.id}"
