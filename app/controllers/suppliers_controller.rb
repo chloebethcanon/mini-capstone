cc2d2ae6b4237f87a97376d0223e4b59801825db
chloebethcanon <chloebethcanon@gmail.com>
2015-12-06 10:49:09 -0600

    Updated supplier routes, Updated suppliers new.html.erb file, Updated methods in suppliers controller, created index and show pages

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,6 +1,11 @@
 class SuppliersController < ApplicationController
 
+  def index
+    @suppliers = Supplier.all
+  end
+
   def show
+    @supplier = Supplier.find_by(id: params[:id])
   end
 
   def new
@@ -8,6 +13,8 @@ class SuppliersController < ApplicationController
   end
 
   def create
+    @supplier = Supplier.create(params[:supplier])
+    redirect_to "/suppliers/#{@supplier.id}"
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
   get '/suppliers' => 'suppliers#index'
   get '/suppliers/new' => 'suppliers#new'
   post '/suppliers' => 'suppliers#create'
-  get '/suppliers/:id' => 'suppliers#show'
+  get '/suppliers/:id' => 'suppliers#show', as: :supplier
   get '/suppliers/:id/edit' => 'suppliers#edit'
   patch '/suppliers/:id' => 'suppliers#update'
   delete '/suppliers/:id' => 'suppliers#destroy'
